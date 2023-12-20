24264e8c868705bb9aad129ef0b4f3f59373c166
Bernard Huff <bernard.huff008@gmail.com>
2023-12-20 02:55:48 +0000

    Added more files to the project.

---
+++ page2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="description" content="Page 2">
+  <meta name="author" content="Your Name">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="page2.css">
+   <title>Page 2</title>
+</head>
+<body>
+  <h1>Page 2</h1>
+  <p>
+    <?php
+      echo "Hello, " . $_GET['name'] . "!";
+    ?>
+  </p>
+</body>
+ <script src="page2.js"></script>
+
+</html>
